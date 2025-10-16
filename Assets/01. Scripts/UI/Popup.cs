f852f126c9d233f388bc1f43febc9c7f4a0c58ce
Crogen0407 <mymcoding0407@gmail.com>
2025-10-16 11:24:39 +0900

    feat : namespace path

---
+++ Assets/01. Scripts/UI/Popup.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Project_Lamb.UI.Popups
+{
+    public abstract class Popup : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
